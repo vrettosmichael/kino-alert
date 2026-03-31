4c59e591fe80ef8dc6793a67633bd1b0adf459ef
vrettosmichael <52506104+vrettosmichael@users.noreply.github.com>
2026-03-31 17:45:07 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import time
+import requests
+
+TOKEN = "VALE_TO_TOKEN_SOU"
+CHAT_ID = "VALE_TO_CHAT_ID_SOU"
+
+last_draw_id = None
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, data=data)
+
+while True:
+    try:
+        res = requests.get("https://api.opap.gr/draws/v3.0/1100/last-result")
+        data = res.json()
+
+        draw_id = data["drawId"]
+        draw_time = data["drawTime"]
+
+        if draw_id != last_draw_id:
+            last_digit = draw_id % 10
+
+            if last_digit == 3 or last_digit == 4:
+                send_message(f"🚨 ΚΙΝΟ ALERT\nΣειρά: {last_digit}\nΏρα: {draw_time}")
+
+        last_draw_id = draw_id
+
+    except Exception as e:
+        print("Error:", e)
+
+    time.sleep(300)
