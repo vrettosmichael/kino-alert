248e10ab718cd7e7a044509e8e1bc6f471820efd
vrettosmichael <52506104+vrettosmichael@users.noreply.github.com>
2026-04-08 12:36:08 +0300

    Update app.py
    fix drawId path

--- app.py
+++ app.py
@@ -16,14 +16,13 @@ def send_message(text):
     print(response.text)
 
 try:
-    res = requests.get("https://api.opap.gr/draws/v3.0/1100/last-result-and-active")
-    data = res.json()
+   res = requests.get("https://api.opap.gr/draws/v3.0/1100/last-result-and-active")
+data = res.json()
 
-    draw_id = data["drawId"]
-    draw_time_raw = data["drawTime"]
+last = data["last"]
 
-    draw_time = datetime.fromtimestamp(draw_time_raw / 1000).strftime("%H:%M")
-    last_digit = draw_id % 10
+draw_id = last["drawId"]
+draw_time_raw = last["drawTime"]
 
     if last_digit == 3 or last_digit == 4:
         send_message(f"🚨 ΚΙΝΟ ALERT\nΣειρά: {last_digit}\nΏρα: {draw_time}")
