e32d610ededa855a219c03adfc453b9854c64281
vrettosmichael <52506104+vrettosmichael@users.noreply.github.com>
2026-06-23 17:39:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
+import os
 import requests
 
-url = "https://api.opap.gr/draws/v3.0/1100/last-result-and-active"
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
-r = requests.get(url)
-data = r.json()
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": text
+    })
 
-print(data)
+try:
+    # Παίρνει το τελευταίο αποτέλεσμα
+    r = requests.get(
+        "https://api.opap.gr/draws/v3.0/1100/last-result-and-active",
+        timeout=10
+    )
+
+    data = r.json()
+
+    print("LATEST DRAW:")
+    print(data["last"]["drawId"])
+
+    # ΠΡΟΣΩΡΙΝΟ TEST
+    send_message("Teleperformance TEST")
+
+except Exception as e:
+    print("ERROR:", e)
